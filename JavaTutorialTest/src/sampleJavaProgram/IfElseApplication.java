968d035d45d3f1bc2dd33f987b040fa1a65d7a04
abhij <abhij@LAPTOP-SIL5AQEA>
2018-08-29 20:49:32 +0530

    Adding new class
    Signed-off-by: abhijit <PALABHIBKP@GMAIL.COM>

---
+++ JavaTutorialTest/src/sampleJavaProgram/IfElseApplication.java
@@ -0,0 +1,15 @@
+package sampleJavaProgram;
+
+public class IfElseApplication {
+	
+	public static void main(String[] args){
+		int i=20;
+		if(i>30){
+			System.out.println("Passed!!");
+		}
+		else{
+			System.out.println("Failed!!");
+		}
+	}
+
+}
